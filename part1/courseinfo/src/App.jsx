4ecdedd58387d7b982d9e45dec31d5d553116330
diogofriggo <diogofriggobr@gmail.com>
2023-09-02 22:13:07 +0200

    ex1.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,3 +1,10 @@
+const Header = (props) => <h1>{props.course}</h1> 
+
+// not a fan of too much horizontal span, just testing whether I could do this
+const Content = (props) => props.entries.map((e, i) => <p key={i}>{e.name} {e.count}</p>)
+
+const Total = (props) => <p>Number of exercises {props.entries.length}</p>
+
 const App = () => {
     const course = "Half Stack application development"
     const part1 = "Fundamentals of React"
@@ -6,20 +13,18 @@ const App = () => {
     const exercises2 = 7
     const part3 = "State of a component"
     const exercises3 = 14
+   
+    var entries = [
+        {name: part1, count: exercises1},
+        {name: part2, count: exercises2},
+        {name: part3, count: exercises3},
+    ]
 
     return (
         <div>
-            <h1>{course}</h1> 
-            <p>
-                {part1} {exercises1}
-            </p>
-            <p>
-                {part2} {exercises2}
-            </p>
-            <p>
-                {part3} {exercises3}
-            </p>
-            <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+            <Header course={course} />
+            <Content entries={entries}/>
+            <Total entries={entries}/>
         </div>
     )
 }
