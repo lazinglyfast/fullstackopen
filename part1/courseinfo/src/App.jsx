c90b066715f2cdb48f5749e5c25542f6c83a73d4
lazinglyfast <lazinglyfast@gmail.com>
2023-09-03 14:23:19 +0200

    ex1.3

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -19,18 +19,22 @@ Total.propTypes = {
 
 const App = () => {
   const course = "Half Stack application development"
-  const part1 = "Fundamentals of React"
-  const exercises1 = 10
-  const part2 = "Using props to pass data"
-  const exercises2 = 7
-  const part3 = "State of a component"
-  const exercises3 = 14
+  const part1 = {
+    name: "Fundamentals of React",
+    exercises: 10,
+  }
 
-  var entries = [
-    { name: part1, count: exercises1 },
-    { name: part2, count: exercises2 },
-    { name: part3, count: exercises3 },
-  ]
+  const part2 = {
+    name: "Using props to pass data",
+    exercises: 7,
+  }
+
+  const part3 = {
+    name: "State of a component",
+    exercises: 14,
+  }
+
+  var entries = [part1, part2, part3]
 
   return (
     <>
