cff019e12d66706b5976857e6c5046c021afce4e
lazinglyfast <lazinglyfast@gmail.com>
2023-09-03 15:01:10 +0200

    ex1.5

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,46 +1,52 @@
-import PropTypes from 'prop-types'; // ES6
-// not a fan of too much horizontal span
-// just testing whether this is valid syntax
-const Header = (props) => <h1>{props.course}</h1>
-Header.propTypes = {
-  course: PropTypes.string
-}
+import PropTypes from 'prop-types';
 
-let partsPropTypes = {
-  parts: PropTypes.arrayOf(
-    PropTypes.shape({ name: PropTypes.string, exercises: PropTypes.number })
+let coursePropTypes = {
+  course: PropTypes.shape(
+    {
+      name: PropTypes.string,
+      parts: PropTypes.arrayOf(
+        PropTypes.shape({ name: PropTypes.string, exercises: PropTypes.number })
+      )
+    }
   )
 }
 
-const Content = (props) => {
-  return (props.parts.map((part, i) => <p key={i}>{part.name} {part.exercises}</p>))
-}
-Content.propTypes = partsPropTypes
+const Header = (props) => <h1>{props.course.name}</h1>
+Header.propTypes = coursePropTypes
 
-const Total = (props) => <p>Number of exercises {props.parts.length}</p>
-Total.propTypes = partsPropTypes
+const Content = (props) => {
+  return props.course.parts.map((part, i) => {
+    return <p key={i}>{part.name} {part.exercises}</p>
+  })
+}
+Content.propTypes = coursePropTypes
+
+const Total = (props) => <p>Number of exercises {props.course.parts.length}</p>
+Total.propTypes = coursePropTypes
 
 const App = () => {
-  const course = "Half Stack application development"
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    }
-  ]
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      }
+    ],
+  }
   return (
     <>
       <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Content course={course} />
+      <Total course={course} />
     </>
   )
 }
