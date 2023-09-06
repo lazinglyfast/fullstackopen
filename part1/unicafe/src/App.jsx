81ecabb1a8dcf69c01e269231b852778552bcc0f
lazinglyfast <lazinglyfast@gmail.com>
2023-09-06 23:01:26 +0200

    ex1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,7 +6,12 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  const all = good + neutral + bad
+  const feedback = {
+    good: good,
+    neutral: neutral,
+    bad: bad,
+    all: good + neutral + bad
+  }
 
   return (
     <div>
@@ -15,14 +20,34 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <h2>statistics</h2>
+      <Leaderboard feedback={feedback} />
+    </div>
+  )
+}
+
+const Leaderboard = ({ feedback }) => {
+  const { all, good, neutral, bad } = feedback
+  if (all === 0) {
+    return <p> No feedback given </p>
+  }
+  return (
+    <>
       <Statistics stat={good} name="good" />
       <Statistics stat={neutral} name="neutral" />
       <Statistics stat={bad} name="bad" />
       <Statistics stat={all} />
       <Statistics stat={(good - bad) / all} />
       <Statistics stat={good / all} displayAsPercentage />
-    </div>
+    </>
   )
+}
+Leaderboard.propTypes = {
+  feedback: PropTypes.shape({
+    all: PropTypes.number,
+    good: PropTypes.number,
+    neutral: PropTypes.number,
+    bad: PropTypes.number,
+  }),
 }
 
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
@@ -32,9 +57,6 @@ Button.propTypes = {
 }
 
 const Statistics = ({ stat, name, displayAsPercentage }) => {
-  if (isNaN(stat)) {
-    return <p> Waiting on feedback to compute statistics... </p>
-  }
   if (displayAsPercentage) {
     return <p> {name} {stat * 100} % </p>
   }
