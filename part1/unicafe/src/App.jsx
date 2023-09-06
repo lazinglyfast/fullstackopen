464fb1e9ea7accbbaebd8d44f07c7a7564749522
lazinglyfast <lazinglyfast@gmail.com>
2023-09-06 22:29:02 +0200

    ex1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -6,16 +6,21 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
+  const all = good + neutral + bad
+
   return (
     <div>
-      <h1>give feedback</h1>
+      <h2>give feedback</h2>
       <Button handleClick={() => setGood(good + 1)} text="good" />
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
-      <h1>statistics</h1>
+      <h2>statistics</h2>
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <Stat stat={(good - bad) / all} />
+      <Stat stat={good / all} displayAsPercentage />
     </div>
   )
 }
@@ -24,6 +29,20 @@ const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</
 Button.propTypes = {
   handleClick: PropTypes.func,
   text: PropTypes.string,
+}
+
+const Stat = ({ stat, displayAsPercentage }) => {
+  if (isNaN(stat)) {
+    return <p> Waiting on feedback to compute statistics... </p>
+  }
+  if (displayAsPercentage) {
+    return <p> {stat * 100} % </p>
+  }
+  return <p> {stat} </p>
+}
+Stat.propTypes = {
+  stat: PropTypes.number,
+  displayAsPercentage: PropTypes.bool,
 }
 
 export default App
