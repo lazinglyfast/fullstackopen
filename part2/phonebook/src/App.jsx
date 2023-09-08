9a6f5c1e7fa307d23669e133929a1bedfbd19207
lazinglyfast <lazinglyfast@gmail.com>
2023-09-08 22:18:55 +0200

    ex2.9

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -2,11 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "040-1234567" },
-    { name: "teddy", number: "613-1234567" },
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
   ])
   const [newName, setNewName] = useState("")
   const [newNumber, setNewNumber] = useState("")
+  const [query, setQuery] = useState(new RegExp())
 
   const addPerson = (event) => {
     event.preventDefault()
@@ -14,7 +17,11 @@ const App = () => {
     if (entryAlreadyExists) {
       alert(`${newName} already exists`)
     } else {
+      const largestId = persons.reduce((max, person) => {
+        person.id > max ? person.id : max
+      }, Number.MIN_VALUE)
       setPersons(persons.concat({
+        id: largestId + 1,
         name: newName,
         number: newNumber
       }))
@@ -29,10 +36,19 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
+  const onFilterChange = (event) => {
+    // performance hit
+    setQuery(new RegExp(event.target.value, "i"))
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with: <input onChange={onFilterChange} />
+      </div>
       <form onSubmit={addPerson}>
+        <h2>Add new</h2>
         <div>
           name: <input onChange={onNameChange} />
         </div>
@@ -44,7 +60,14 @@ const App = () => {
         </div>
       </form >
       <h2>Numbers</h2>
-      {persons.map((person) => <div key={person.name}>{person.name} {person.number}</div>)}
+      {
+        persons
+          .filter((person) => {
+            return query.test(person.name)
+          })
+          .map((person) => {
+            return <div key={person.id}>{person.name} {person.number}</div>
+          })}
     </div >
   )
 }
