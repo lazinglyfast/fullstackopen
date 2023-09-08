55d1a1d94db3804825f690baa95286c29269913d
lazinglyfast <lazinglyfast@gmail.com>
2023-09-08 16:20:21 +0200

    ex2.4

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,29 +1,56 @@
 import PropTypes from 'prop-types';
 
 const App = () => {
-  const course = {
-    id: 1,
-    name: "Half Stack application development",
-    parts: [
-      {
-        name: "Fundamentals of React",
-        exercises: 10,
-        id: 1,
-      },
-      {
-        name: "Using props to pass data",
-        exercises: 7,
-        id: 2,
-      },
-      {
-        name: "State of a component",
-        exercises: 14,
-        id: 3,
-      }
-    ],
-  }
+  const courses = [
+    {
+      id: 1,
+      name: "Half Stack application development",
+      parts: [
+        {
+          name: "Fundamentals of React",
+          exercises: 10,
+          id: 1,
+        },
+        {
+          name: "Using props to pass data",
+          exercises: 7,
+          id: 2,
+        },
+        {
+          name: "State of a component",
+          exercises: 14,
+          id: 3,
+        },
+        {
+          name: 'Redux',
+          exercises: 11,
+          id: 4
+        }
+      ]
+    },
+    {
+      name: 'Node.js',
+      id: 2,
+      parts: [
+        {
+          name: 'Routing',
+          exercises: 3,
+          id: 1
+        },
+        {
+          name: 'Middlewares',
+          exercises: 7,
+          id: 2
+        }
+      ]
+    }
+  ]
+
   return (
-    <Course course={course} />
+    <div>
+      <h1>Web development curriculum</h1>
+      {courses.map((course) => <Course key={course.id} course={course} />)}
+    </div>
   )
 }
 
@@ -37,7 +64,7 @@ const Course = ({ course }) => {
   )
 }
 
-const Header = ({ course }) => <h1>{course.name}</h1>
+const Header = ({ course }) => <h2>{course.name}</h2>
 
 const Content = ({ course }) => {
   return course.parts.map((part) => {
@@ -50,10 +77,12 @@ const Part = ({ part }) => {
 }
 
 const Total = ({ course }) => {
+  const exercises = course.parts.reduce((sum, part) => {
+    return sum + part.exercises
+  }, 0)
+
   return (
-    <p>Number of exercises {course.parts.reduce((sum, part) => {
-      return sum + part.exercises
-    }, 0)}</p>
+    <b>total of {exercises} exercises</b>
   )
 }
 
